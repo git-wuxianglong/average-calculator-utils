ac6e76ea56d9b9ae8956c28ba5bffbdc6fdb212b
San <wuxlpersonal@gmail.com>
2024-05-09 20:56:35 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 
+const average = require('./average.js');
+
+// 定义测试用例
+const test1 = [1, 2, 3, 4, 5];
+const test2 = [10, 20, 30, 40, 50];
+const test3 = [2, 2, 2, 2, 2];
+
+console.log(average(test1)); // 输出：3
+console.log(average(test2)); // 输出：30
+console.log(average(test3)); // 输出：2
